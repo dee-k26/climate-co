48ebcf7bb0d9dc8e4b81276430d6d5afdc561f1e
Daniel Knight <dan@knight-co.au>
2023-08-18 16:43:00 +1000

    trying again

--- src/components/LoadingPage/LoadingPage.js
+++ src/components/LoadingPage/LoadingPage.js
@@ -32,7 +32,6 @@ function LoadingPage() {
   return (
     <div className="App">
     <Header />
-    <Home />
     <Routes>
       <Route path="/climate-co" element={<Home />} /> 
       <Route path="/about" element={<AboutUs />} />
